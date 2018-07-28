f53363a2e966f8883c24fe5f11ec7cbb7684996c
niekas <albertas.gimbutas@mii.vu.lt>
2018-07-28 11:04:32 +0300

    Add django-extensions module

--- plates/plates/settings.py
+++ plates/plates/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '8m8fge&_i$6x6jj04s#)dgg^v!_qynb$0rdox4%whfj4lrrs)_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'rest_framework',
     'plates',
 ]
