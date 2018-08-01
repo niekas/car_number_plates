5f59bb7078619b211e5c990faa6960a65e5cd2c8
niekas <albertas.gimbutas@mii.vu.lt>
2018-08-01 07:31:11 +0300

    Add input validation for NumberPlates

--- plates/plates/api.py
+++ plates/plates/api.py
@@ -1,7 +1,12 @@
+import re
+
 from rest_framework import serializers, viewsets
 from rest_framework import filters
 
 from plates.models import Owner, NumberPlate
+
+only_letters_and_numbers = re.compile("^[a-zA-Z0-9]*$")
+
 
 
 class OwnerSerializer(serializers.HyperlinkedModelSerializer):
@@ -19,6 +24,13 @@ class NumberPlateSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = NumberPlate
         fields = '__all__'
+
+    def validate(self, data):
+        if not only_letters_and_numbers.match(data['number']):
+            raise serializers.ValidationError("Only english letters and numbers "
+                                              "are allowed in car number plates")
+        return data
+
 
 
 class OwnerViewSet(viewsets.ModelViewSet):
