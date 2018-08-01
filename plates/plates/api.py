c6a0e4bc07e7e96ac995751407cb14765079f1e3
niekas <albertas.gimbutas@mii.vu.lt>
2018-08-01 14:57:23 +0300

    Require owner (first_name, last_name) to be unique

--- plates/plates/api.py
+++ plates/plates/api.py
@@ -2,10 +2,10 @@ import re
 
 from rest_framework import serializers, viewsets
 from rest_framework import filters
+from rest_framework.views import exception_handler
+from rest_framework.validators import UniqueTogetherValidator
 
 from plates.models import Owner, NumberPlate
-
-only_letters_and_numbers = re.compile("^[a-zA-Z0-9]*$")
 
 
 
@@ -16,6 +16,13 @@ class OwnerSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Owner
         fields = '__all__'
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Owner.objects.all(),
+                fields=('first_name', 'last_name'),
+                message='Owner already exists with this first and last names'
+            )
+        ]
 
 
 class NumberPlateSerializer(serializers.HyperlinkedModelSerializer):
@@ -25,11 +32,16 @@ class NumberPlateSerializer(serializers.HyperlinkedModelSerializer):
         model = NumberPlate
         fields = '__all__'
 
-    def validate(self, data):
-        if not only_letters_and_numbers.match(data['number']):
+    def validate_number(self, value):
+        # There are a lot of different formats of plate numbers, see:
+        # https://regitra.lt/lt/paslaugos-ir-veikla/numerio-zenklai/numerio-zenklu-tipai
+        # So a general rule for lithuanian number plate format is applied
+        only_letters_and_numbers = re.compile("^[a-zA-Z0-9]*$")
+
+        if not only_letters_and_numbers.match(value):
             raise serializers.ValidationError("Only english letters and numbers "
                                               "are allowed in car number plates")
-        return data
+        return value
 
 
 
