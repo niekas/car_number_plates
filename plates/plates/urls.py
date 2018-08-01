51e91f75f8d293e49bf2b08c5bb71fb52f15e22e
niekas <albertas.gimbutas@mii.vu.lt>
2018-08-01 19:20:03 +0300

    Store page CSS style in a separate file
    Also use Django helper to serve static files.

--- plates/plates/urls.py
+++ plates/plates/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -16,4 +18,4 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
